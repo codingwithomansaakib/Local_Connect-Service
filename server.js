9a246dbb5a3296b0e071d5e782db012337e347ff
Oman saakib <150897476+codingwithomansaakib@users.noreply.github.com>
2025-07-08 15:23:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use('/api', bookingRoutes);
 app.use('/api', contactRoutes);
 
 // ✅ 4. Start server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
